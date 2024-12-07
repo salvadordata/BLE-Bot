9c338bd453feaea7c3404c9c1071c209749278b8
salvadordata <ak987tbram@icloud.com>
2024-12-07 17:56:17 -0500

    Update BLESpam.h

--- lib/modules/BLESpam.h
+++ lib/modules/BLESpam.h
@@ -1,12 +1,24 @@
 #ifndef BLESPAM_H
 #define BLESPAM_H
 
+#include <string>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
 class BLESpam {
 public:
-  static void start();  // Start BLE Spam
+    // Initialize the BLE Spam system
+    static void start(const std::string& deviceName = "BLESpam");
 
 private:
-  static void initializeAdvertisingData();  // Initialize random advertising data
+    static BLEServer* pServer;               // BLE Server instance
+    static BLEAdvertising* pAdvertising;    // BLE Advertising instance
+
+    // Internal methods
+    static void initializeBLE(const std::string& deviceName);  // Initializes BLE
+    static void initializeAdvertisingData();                   // Sets up advertising data
+    static void startAdvertising();                            // Starts advertising
 };
 
-#endif
+#endif // BLESPAM_H
