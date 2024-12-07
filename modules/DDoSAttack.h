b2e36631a17e8f5b7ba667b56840b853b4d38e5a
salvadordata <ak987tbram@icloud.com>
2024-12-07 16:38:16 -0500

    Update DDoSAttack.h

---
+++ modules/DDoSAttack.h
@@ -0,0 +1,10 @@
+#ifndef DDOSATTACK_H
+#define DDOSATTACK_H
+
+class DDoSAttack {
+public:
+  static void start();            // Start the DDoS Attack
+  static void initializeUUIDs();  // Generate random UUIDs for advertising
+};
+
+#endif
